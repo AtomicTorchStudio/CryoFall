eb5f90d77ec9c681d79a7873bfc2e12b3d875bf8
Peter <petergorman11@gmail.com>
2021-05-10 14:23:18 -0400

    Add note about odd numbers for hotbar slots constant

--- Core.cpk/Scripts/Characters/State/PlayerConstants.cs
+++ Core.cpk/Scripts/Characters/State/PlayerConstants.cs
@@ -10,6 +10,7 @@
         /// <summary>
         /// The number of the hotbar slots is configured here.
         /// Please note that it doesn't apply to the already registered players (their hotbar container is already created).
+        /// Also note if this value is odd then it will create a graphical bug in the display of the hotbar. See https://i.imgur.com/Dl6sRBi.png
         /// </summary>
         public const int HotbarSlotsCount = 10;
     }
